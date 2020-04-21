18f8840df94395fddbfc48136746a62ebc0eafeb
fkash850 <56698702+fkash850@users.noreply.github.com>
2020-04-21 15:30:19 -0400

    Add files via upload

---
+++ Minor.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Minor {
+    String name;
+    int creditsNeeded;
+    
+    public Minor() {
+        
+    }
+    
+    public String getMinorName() {
+        return "";
+    }
+    
+    public int getCreditsNeeded() {
+        return creditsNeeded;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,22 @@
+package example;
+
+import java.util.*;
+
+public class Student {
+    String name;
+    Major major;
+    boolean isUndergraduate;
+    boolean isGraduate;
+    
+    public Student() {
+        
+    }
+    
+    public String getStudentName() {
+        return "";
+    }
+    
+    public String toString() {
+        return "";
+    }
+}
